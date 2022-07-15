947e6326a529e49c2af6ab6e59ace91e246124c8
usmon <usmonovahulkar1999@gmail.com>
2022-07-15 17:32:39 +0500

    jwhgf

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ function getCustomer(id : number): Customer | null{
     return id === 0? null :{birthday: new Date()};
 
 }
-let customer= getCustomer(0);
+let customer= getCustomer(5);
 console.log(customer?.birthday?.getFullYear());
 
 let log : any = null;
